79e29a38469965370702eb150a39cb75b0c23dd8
fahmi ahmad <fahmiahmad@tutanota.com>
2021-12-12 19:38:58 +0700

    update client

---
+++ client/pages/tickets/new.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
+
+const NewTicket = () => {
+  const [title, titleSet] = useState("");
+  const [price, priceSet] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: {
+      title,
+      price,
+    },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    priceSet(value.toFixed(2));
+  };
+
+  return (
+    <div>
+      <h1>Create a Ticket</h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => titleSet(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input
+            value={price}
+            onChange={(e) => priceSet(e.target.value)}
+            onBlur={onBlur}
+            className="form-control"
+          />
+        </div>
+        {errors}
+        <button className="btn btn-summary" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTicket;
