9b3ae9bd46f06046befec8c8a8e73dca349cb5e5
fahmi ahmad <fahmiahmad@tutanota.com>
2021-12-14 19:39:38 +0700

    mongo session

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
-import cookieSession from "cookie-session";
+import MongoStore from "connect-mongo";
+import session from "express-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -12,10 +13,19 @@ const app = express();
 app.set("trust proxy", true);
 app.use(express.json());
 app.use(
-  cookieSession({
+  session({
     name: "ticketing_session",
-    signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    secret: process.env.JWT_KEY!,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI_SESSION,
+    }),
+    cookie: {
+      httpOnly: true,
+      sameSite: "lax",
+      // secure: true,
+      // expires: expiryDate,
+      // maxAge: 60000 * 2, // 2 minutes
+    },
   })
 );
 
